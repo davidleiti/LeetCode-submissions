728ba4555f1174e42df896ee6a0324fb6e5353ad
David Leiti <leiti26@gmail.com>
2021-12-04 13:31:49 +0200

    ReverseString

---
+++ Strings/ReverseString/ReverseString.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun reverseString(s: CharArray): Unit {
+        var start = 0
+        var end = s.size - 1
+        while (start < end) { 
+            val temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start++
+            end--
+        }
+    }
+}
